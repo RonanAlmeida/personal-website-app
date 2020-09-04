24aeea33a43d4d3d3da9ca3960f4d98d2d7aa768
RonanAlmeida <ronancalmeida@hotmail.com>
2020-09-04 16:24:09 -0400

    Updates

---
+++ precache-manifest.67f5cfc383cb2a37f93525d84d55e77a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5123cab915055351ca0c1c220ee8a6f",
+    "url": "/personal-website-app/index.html"
+  },
+  {
+    "revision": "137f0f62bced0efee2cf",
+    "url": "/personal-website-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "61da4d738e199332b4bf",
+    "url": "/personal-website-app/static/js/2.aef1bd1f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/personal-website-app/static/js/2.aef1bd1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "137f0f62bced0efee2cf",
+    "url": "/personal-website-app/static/js/main.da2a6a32.chunk.js"
+  },
+  {
+    "revision": "1b2de527e28479953511",
+    "url": "/personal-website-app/static/js/runtime-main.6fb5e1d9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/personal-website-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
