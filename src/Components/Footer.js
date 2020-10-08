c45bca5d87352d7848428af508bb71a324d67fc5
RonanAlmeida <ronancalmeida@hotmail.com>
2020-10-08 15:46:54 -0400

    bottom icons added with links

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Container, List, Icon } from "semantic-ui-react";
+import pdf from "../Documents/RonanAlmeidaResume.pdf";
 
 function Footer() {
   const Item = {
@@ -8,7 +10,55 @@ function Footer() {
     marginTop: "10em",
     marginBottom: "1em",
   };
-  return <div style={Item}>Designed and Developed by Ronan Almeida © 
-  </div>;
+
+  // return <div style={Item}>Designed and Developed by Ronan Almeida ©
+  // </div>;
+  function onResumeClick() {
+    window.open(pdf);
+  }
+  return (
+    <Container style={Item} textAlign="center">
+      {/* <Divider inverted section /> */}
+      {/* <Image src="/logo.png" centered size="mini" /> */}
+      <List horizontal divided link size="small">
+        <List.Item as="a">Designed and Developed by Ronan Almeida ©</List.Item>
+        <List.Item
+          as="a"
+          target="_blank"
+          onClick={onResumeClick}
+        >
+          <Icon color="black" size="small" name="file alternate" />
+          Resume
+        </List.Item>
+
+        <List.Item
+          as="a"
+          target="_blank"
+          href="https://github.com/RonanAlmeida"
+        >
+          <Icon color="black" size="small" name="github" />
+          Github
+        </List.Item>
+
+        <List.Item
+          as="a"
+          target="_blank"
+          href="https://www.linkedin.com/in/ronanalmeida/"
+        >
+          {" "}
+          <Icon color="black" size="small" name="linkedin" />
+          LinkedIn
+        </List.Item>
+        <List.Item
+          as="a"
+          target="_blank"
+          href="mailto: ronan.almeida@queensu.ca"
+        >
+          <Icon color="black" size="small" name="mail" />
+          Email
+        </List.Item>
+      </List>
+    </Container>
+  );
 }
 export default Footer;
